d3f61594b7f53d3ca54d2e8388c3b2fb8955cfb4
Pepos Carmona <peposcarmona@gmail.com>
2017-03-23 16:23:20 -0500

    reindent

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 window.onload = function() {
 
 	document.getElementById("addDatasetBtn").onclick = function(e) {
-        e.preventDefault();
-        const file = document.getElementById('datasetFile').files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.readAsText(file);
-            reader.onload = function(e) {
-            	json = JSON.parse(e.target.result);
-            	const dataset = new Dataset(json);
-                const parent = document.getElementById('parentHolder');
-                const tableRenderer = new TableRenderer(parent, dataset);
-                tableRenderer.renderTable();
-            }
-        }else{
-        	alert("You must to select a file!");
-        }
-    };
+		e.preventDefault();
+		const file = document.getElementById('datasetFile').files[0];
+		if (file) {
+			const reader = new FileReader();
+			reader.readAsText(file);
+			reader.onload = function(e) {
+				json = JSON.parse(e.target.result);
+				const dataset = new Dataset(json);
+				const parent = document.getElementById('parentHolder');
+				const tableRenderer = new TableRenderer(parent, dataset);
+				tableRenderer.renderTable();
+			}
+		}else{
+			alert("You must to select a file!");
+		}
+	};
 }
