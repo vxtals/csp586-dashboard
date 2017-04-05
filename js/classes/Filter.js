63d95301ee91f3e145a683a5eb0dca6b51bfb46e
vxtals <vxtals@gmail.com>
2017-04-04 22:42:01 -0500

    Added Filter class skeleton

---
+++ js/classes/Filter.js
@@ -0,0 +1,23 @@
+'use strict';
+class Filter {
+  constructor(dataset){
+    this.dataset = dataset;   
+    this.df = new DataFrame(dataset.rows, dataset.columnNames);
+  }
+
+  filterByColumn(){
+
+  }
+
+  filterByRow(){
+
+  }
+
+  filterByParameter(){
+
+  }
+
+  filterByQuery(){
+
+  }
+}
