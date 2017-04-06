57d4264100a92f454100d14f6c97a51ce7e93729
vxtals <vxtals@gmail.com>
2017-04-05 20:52:12 -0500

    Adapted to work with dataset classes. Implemented filterByColumn

--- js/classes/Filter.js
+++ js/classes/Filter.js
@@ -1,12 +1,14 @@
 'use strict';
 class Filter {
   constructor(dataset){
-    this.dataset = dataset;   
-    this.df = new DataFrame(dataset.rows, dataset.columnNames);
+    this.dataset = dataset; 
+    this.df = this.dataset.dataframe 
   }
 
-  filterByColumn(){
-
+  filterByColumn(selectedColumns){
+    let filteredDataset = new Dataset()
+    filteredDataset.setDataframe(this.df.select(...selectedColumns))
+    return filteredDataset
   }
 
   filterByRow(){
