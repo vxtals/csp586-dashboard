5703a0adff71cb95f5d2f5426a172db0348ee54e
Pepos Carmona <peposcarmona@gmail.com>
2017-04-07 16:16:55 -0500

    add buttons

--- js/main.js
+++ js/main.js
@@ -17,4 +17,42 @@ window.onload = function() {
 			alert("You must to select a file!");
 		}
 	};
+
+	document.getElementById("barChart").onclick = function(e) {
+		e.preventDefault();
+		ctx = null;
+		myChart = null;
+		ctx = document.getElementById("myChart");
+    myChart = new BarChart(ctx);
+    myChart.addValues(
+      ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      [12, 19, 3, 5, 2, 3],
+      [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+      ]
+    );
+    myChart.addValue('Violet', 9, 'rgba(255, 0, 64, 1)');
+    myChart.displayChart(ctx);
+    myChart.setLabel('Mon CHAR');
+	};
+
+	document.getElementById("lineChart").onclick = function(e) {
+		e.preventDefault();
+		ctx = null;
+		myChart = null;
+		ctx = document.getElementById("myChart");
+		myChart = new LineChart(ctx);
+		myChart.addValues(
+			['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+			[12, 19, 3, 5, 2, 3]
+		);
+		myChart.addValue('Violet', 9);
+		myChart.displayChart(ctx);
+		myChart.setLabel('Mon CHAR');
+	};
 }
