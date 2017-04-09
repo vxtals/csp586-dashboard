e680e5ed843d97cbfbfbd9aaa2055ef20a5c8e55
Pepos Carmona <peposcarmona@gmail.com>
2017-04-09 17:10:12 -0500

    merge without error

--- js/classes/Dataset.js
+++ js/classes/Dataset.js
@@ -1,43 +1,58 @@
 class Dataset {
 
-  constructor(json) {
-    if(!!json){
-      this.columns = this.parseColumns(json.meta.view.columns);
-      this.rows = this.parseRows(json.data);
-      this.dataframe = new DataFrame(this.rows, this.columns.map(
-        function(col){
-          return col.name
+    constructor(json) {
+        if(!!json){
+            this.columns = this.parseColumns(json.meta.view.columns);
+            this.rows = this.parseRows(json.data);
+            this.dataframe = new DataFrame(this.rows, this.columns.map(
+                function(col){
+                    return col.name
+                }
+            ))
         }
-      ))
     }
 
-    // Sort columns by position
-    columns.sort(function(a, b) {
-      return a.position - b.position;
-    });
+    parseColumns(columnsArray){
+        const columns = [];
+        for (let i = 0; i < columnsArray.length; i++) {
+            const column = columnsArray[i];
+            if(column.id >= 0){
+                column.dataPosition = i;
+                columns.push(column);
+            }
+        }
 
-    /*for(let i = 0; i < columns.length; i++){
-    alert(columns[i].name);
-  }*/
+        // Sort columns by position
+        columns.sort(function(a, b) {
+            return a.position - b.position;
+        });
 
-  return columns;
-}
+        /*for(let i = 0; i < columns.length; i++){
+            alert(columns[i].name);
+        }*/
 
-parseRows(rowsArray){
-  const rows = [];
-
-  for(let i = 0; i < rowsArray.length; i++){
-    const currentRow = rowsArray[i];
-    const row = [];
-    for(let j = 0; j < this.columns.length; j++){
-      row.push(currentRow[this.columns[j].dataPosition]);
+        return columns;
     }
-    rows.push(row);
-  }
 
-  setDataframe(dataframe){
-    this.dataframe = dataframe;
-    this.columns = dataframe.listColumns()
-    this.rows = dataframe.toArray()
-  }
+    parseRows(rowsArray){
+        const rows = [];
+
+        for(let i = 0; i < rowsArray.length; i++){
+            const currentRow = rowsArray[i];
+            const row = [];
+            for(let j = 0; j < this.columns.length; j++){
+                row.push(currentRow[this.columns[j].dataPosition]);
+            }
+            rows.push(row);
+        }
+
+        return rows;
+    }
+
+    setDataframe(dataframe){
+        this.dataframe = dataframe;
+        this.columns = dataframe.listColumns()
+        this.rows = dataframe.toArray()
+    }
+
 }
