3b2a3649be15f58cb6de7c990d1d14134a7b613d
Sergio Penavades <sergio@pnvds.com>
2017-04-14 17:24:04 -0500

    remove comment

--- js/classes/charts/BaseChart.js
+++ js/classes/charts/BaseChart.js
@@ -1,37 +1,8 @@
-// Area and settings configuration of the Chart
 class BaseChart extends ObjectChart {
   constructor() {
     super();
+    // Default Label
     this.chartLabel = '# of Label';
-    /*
-    this.chartArray = {
-        type: this.type,
-        data: {
-            labels: this.chartLabels,
-            datasets: [{
-                label: this.chartLabel,
-                data: this.chartDatas,
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
-        }
-    }
-    if (this.chartColors && this.type == 'bar') {
-      const addColor = {
-        backgroundColor: this.chartColors,
-        borderColor: this.chartColors
-      };
-      this.chartArray.data.datasets.push({addColor});
-    }
-    */
   }
 
   setLabel(label) {
