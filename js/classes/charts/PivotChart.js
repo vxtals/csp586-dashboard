f37d599349b891dff9a86da16e086cf9c47f2bcc
Sergio Penavades <sergio@pnvds.com>
2017-04-29 12:51:58 -0500

    remove some unnecessary options

--- js/classes/charts/PivotChart.js
+++ js/classes/charts/PivotChart.js
@@ -3,10 +3,6 @@ class PivotChart extends StackedChart {
   constructor(ctx) {
     super();
     this.ctx = ctx;
-    this.options.addScales();
-    this.options.addYAxes();
-    this.options.addXAxes();
-    this.options.setStacked('yAxes', true);
     this.options.setStacked('xAxes', true);
   }
 }
--- js/classes/charts/StackedChart.js
+++ js/classes/charts/StackedChart.js
@@ -2,11 +2,7 @@
 class StackedChart extends BarChart {
   constructor(ctx) {
     super();
-    this.ctx = ctx
-    this.options.addScales();
-    this.options.addYAxes();
-    this.options.addTicks('yAxes');
-    this.options.setBeginAtZero('yAxes', true);
+    this.ctx = ctx;
     this.options.setStacked('yAxes', true);
   }
 }
