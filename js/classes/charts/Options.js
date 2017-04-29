ba9587b50a25f074d00e8ca8e9b15ab6efe6e099
vxtals <vxtals@gmail.com>
2017-04-29 00:03:40 -0500

    Functions to add options implemented

--- js/classes/charts/Options.js
+++ js/classes/charts/Options.js
@@ -1,7 +1,11 @@
 // Label, Chart type...
 class Options {
   constructor(options){
-    this.options = options;
+    if(!!options){
+      this.options = options;
+    }else{
+      this.options = {};
+    }
   }
 
   getOptions(){
@@ -21,5 +25,66 @@ class Options {
 
   getDisplayLegend(){
     return this.options.legend.display;
+  }
+
+  addScales(scales){
+    this.options.scales = !!scales ? scale : {};
+  }
+
+  addYAxes(yaxes){
+    if(!this.options.scales){
+      throw 'You must define scales property before';
+    }else{
+      this.options.scales.yAxes = !!yaxes ? yaxes : [{}];
+    }
+  }
+
+  addXAxes(xaxes){
+    if(!this.options.scales){
+      throw 'You must define scales property before';
+    }else{
+      this.options.scales.xAxes = !!xaxes ? xaxes : [{}];
+    }
+  }
+
+  addTicks(axesName, ticks){
+    let validInput = ['yAxes', 'xAxes']
+    if(validInput.indexOf(axesName) < 0){
+      throw 'First argument must be one of' + validInput;
+    }else if(!this.options.scales){
+      throw 'You must define scales property before';
+    }else if(!this.options.scales[axesName]){
+      throw 'You must define ' + axesName + ' property before';
+    }else{
+      this.options.scales[axesName][0].ticks = !!ticks ? ticks : {};
+    }
+  }
+
+  setStacked(axesName, value){
+    let validInput = ['yAxes', 'xAxes']
+    if(validInput.indexOf(axesName) < 0){
+      throw 'First argument must be one of' + validInput
+    }else if(!this.options.scales){
+      throw 'You must define scales property before';
+    }else if(!this.options.scales[axesName]){
+      throw 'You must define ' + axesName + ' property before';
+    }else{
+      this.options.scales[axesName][0].stacked = value;
+    }
+  }
+
+  setBeginAtZero(axesName, value){
+    let validInput = ['yAxes', 'xAxes']
+    if(validInput.indexOf(axesName) < 0){
+      throw 'First argument must be one of' + validInput
+    }else if(!this.options.scales){
+      throw 'You must define scales property before';
+    }else if(!this.options.scales[axesName]){
+      throw 'You must define ' + axesName + ' property before';
+    }else if(!this.options.scales[axesName][0].ticks){
+      throw 'You must define ticks property before';
+    }else{
+      this.options.scales[axesName][0].ticks.beginAtZero = value;
+    }
   }
 }
